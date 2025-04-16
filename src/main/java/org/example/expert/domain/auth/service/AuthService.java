32e32b40118fc71e9a728006902d2ec60d7e0815
dawn0920 <saoul0920@gmail.com>
2025-04-16 21:29:50 +0900

    Lv 1. 코드 개선 1. 코드 개선 퀴즈 - Early Return 문제 해결

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -26,13 +26,37 @@ public class AuthService {
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
 
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+//        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
 
         UserRole userRole = UserRole.of(signupRequest.getUserRole());
 
+        // Early Return
+        // 조건을 만족하면 함수나 메서드에서 일찍 빠져나옴
+
+        // 리팩토링해서 해당 에러가 발생하는 상황일 때,
+        // passwordEncoder의 encode() 동작이 불필요하게 일어나지 않게 코드를 개선
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
+//             String  bearerToken = throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+//             예외는 String로 받을 수 있음 하지만 위의 형식으로는 되지 않음
+//             try {
+//                throw new IllegalArgumentException("에러 발생!");
+//            } catch (IllegalArgumentException e) {
+//                String str = e.getMessage(); // 예외 메시지를 문자열로 꺼냄
+//                System.out.println("예외 메시지: " + str);
+//            }
+//             ❌ 이렇게는 안 됨
+//            try {
+//                throw new RuntimeException("에러 발생");
+//            } catch (String str) { // 컴파일 에러 발생
+//                System.out.println(str);
+//            }
+//             return new SignupResponse(bearerToken);
+//             일반적으로 throw로 예외를 던지면 그 아래에 있는 값들은 모두 실행이 되지 않음
+//             그렇기 때문에 코드를 아래로 바꾸면 불필요하게 일어나지 않음
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
+
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
 
         User newUser = new User(
                 signupRequest.getEmail(),
